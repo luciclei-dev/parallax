b61dbbca0427104123ed398335cfa6c010512dd4
luciclei-dev <lucicleioliveira8@gmail.com>
2025-07-29 21:27:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const ceu = document.querySelector('.ceu')
 
+const ceuDia = document.querySelector('.ceu-dia')
+
 const lua = document.querySelector('.lua')
 
 const montanha = document.querySelector('.montanha')
@@ -12,15 +14,21 @@ const texto = document.querySelector('.texto')
 document.addEventListener('scroll', function() {
     let value = window.scrollY;
 
-    ceu.style.top = -value * 0.3 + 'px';
-
-    lua.style.left = +value * 0.1 +'%';
-     lua.style.top = value * 0.1 +'%';
-
     
-     montanha.style.top= -value * 0.1 + '%';
+     ceu.style.transform = `translateY(${value * 0.5}%)`;
+    
+   ceuDia.style.transform = `translateY(${-value * 0.4}%)`;
 
-    estrada.style.bottom = -value * 0.3 + 'px';
 
-    texto.style.top = value * 0.1 + '%'; // ← agora o texto desce ao rolar
+    lua.style.transform = `translatex(${+value * 0.3}%)`;
+
+     lua.style.top = value * 0.5 +'%';
+
+     
+     montanha.style.transform = `translateY(${-value}px)`;
+
+   const scale = 1 + value * 0.0005; // aumenta o zoom conforme o scroll
+estrada.style.transform = `translateY(${value * 0.1}px) scale(${scale})`;
+
+    texto.style.top = value * 0.2 + '%'; 
 });
